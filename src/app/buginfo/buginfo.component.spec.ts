0101ca854fdc489d20aae89176e7b30772e75f1b
mahajanmalay <malaymahajan.mm@gmail.com>
2022-06-28 19:25:44 +0530

    first commit

---
+++ src/app/buginfo/buginfo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BuginfoComponent } from './buginfo.component';
+
+describe('BuginfoComponent', () => {
+  let component: BuginfoComponent;
+  let fixture: ComponentFixture<BuginfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BuginfoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BuginfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/buginfo/buginfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buginfo',
+  templateUrl: './buginfo.component.html',
+  styleUrls: ['./buginfo.component.css']
+})
+export class BuginfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
